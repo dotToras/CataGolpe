5c9179f28c663d13ca293bf6f7f32873b09b9697
dotToras <odamasenganumeko@gmail.com>
2025-12-06 23:34:45 -0300

    add: módulo básico de Golpes finalizado

--- backend node/controllers/golpeController.js
+++ backend node/controllers/golpeController.js
@@ -1,14 +1,85 @@
-
 import Golpe from '../models/Golpes.js';
 
 
 export const criarGolpes = async (req, res) => {
 
-    try{
+    try {
+
         const golpe = await Golpe.create(req.body);
         res.status(201).json(golpe);
-    }catch(err) {
+
+    } catch(err) {
         return res.status(400).json({ erro: err.message });
     }
 
+}
+
+
+export const buscarGolpes = async (req, res) => {
+
+    try {
+
+        const { tipo, statusVeracidade, votosConfirmacao, votosNegacao} = req.query;
+
+        const filtro = {};
+        const ordenacao = {};
+
+        if(tipo) {
+            filtro.tipo = tipo;
+        }
+
+        if(statusVeracidade) {
+            filtro.statusVeracidade = statusVeracidade;
+        }
+
+        if(votosConfirmacao) {
+            ordenacao.votosConfirmacao = votosConfirmacao;
+        }
+
+        if(votosNegacao) {
+            ordenacao.votosNegacao = votosNegacao;
+        }
+
+
+        const golpes = await Golpe.find(filtro).sort(ordenacao);
+            res.status(200).json({
+            results: golpes.length,
+            data: { golpes },
+         });
+
+    } catch(err) {
+        return res.status(400).json({ erro: err.message });
+    }
+
+}
+
+
+export const votarGolpes = async (req, res) => {
+    try{
+
+        const { id } = req.params
+        const { voto } = req.body
+
+        let votacao = {};
+
+        // verifica qual o tipo de voto
+        if(voto) {
+            if(voto == "confirmacao") {
+                votacao.votosConfirmacao = 1;
+            }
+            if(voto == "negacao"){
+                votacao.votosNegacao = 1;
+            }
+        }
+
+        const golpeVotar = await Golpe.findByIdAndUpdate(
+            id,
+            { $inc : votacao}, // incrementa confirmacao ou negacao a depender do array
+            { new : true}
+        )
+
+        return res.status(200).json(golpeVotar);
+    } catch(err) {
+        return res.status(400).json({ erro: err.message });
+    }
 }
--- backend node/routes/golpeRouter.js
+++ backend node/routes/golpeRouter.js
@@ -2,12 +2,15 @@
 
 import express from 'express';
 import { criarGolpes } from '../controllers/golpeController.js';
+import { buscarGolpes } from '../controllers/golpeController.js';
+import { votarGolpes } from '../controllers/golpeController.js';
 
 const router = express.Router();
 
-
+// Rotas
 router.post('/', criarGolpes);
-
+router.get('/', buscarGolpes);
+router.put('/:id', votarGolpes)
 
 
 export default router;
